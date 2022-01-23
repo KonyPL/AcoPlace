472f0ab73e3a235b4d0b1b1aa8f3c43934539b79
Konrad Trawiński <mrkony98@gmail.com>
2022-01-23 18:07:32 +0100

    Added imageStorage model and endpoint

---
+++ AcoPlaceBackend/src/main/java/put/poznan/AcoPlaceBackend/service/ImageStorageService.java
@@ -0,0 +1,26 @@
+package put.poznan.AcoPlaceBackend.service;
+
+import org.springframework.stereotype.Service;
+import put.poznan.AcoPlaceBackend.exception.ResourceNotFoundException;
+import put.poznan.AcoPlaceBackend.model.ImageStorage;
+import put.poznan.AcoPlaceBackend.repository.ImageStorageRepository;
+
+import java.util.List;
+
+@Service
+public class ImageStorageService {
+
+    private final ImageStorageRepository imageStorageRepository;
+
+    public ImageStorageService(ImageStorageRepository imageStorageRepository) {
+        this.imageStorageRepository = imageStorageRepository;
+    }
+
+    public List<String> getAllAnnouncementImages(Integer announcementId){
+        return imageStorageRepository.findAllByAnnouncementId(announcementId);
+    }
+
+    public ImageStorage saveImage(ImageStorage imageStorage){
+        return imageStorageRepository.save(imageStorage);
+    }
+}
